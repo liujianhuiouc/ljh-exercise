a0b498ae0ee3609c047f134b90ed4eae8f2d5439
liujianhuiouc <liujianhuiouc@163.com>
2015-07-07 16:58:13 +0800

    练习

---
+++ exercise/src/exercise/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package exercise;
+
+/**
+ * 
+ * @author Thinkpad
+ *
+ */
+public class HelloWorld
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("hello");
+	}
+}
